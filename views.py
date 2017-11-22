bb28bff3ef46ffe8dea765dc2bc3cbff4ec39e1d
dizak <dariusz.izak@ibb.waw.pl>
2017-11-22 17:04:20 +0100

    ENH:Removed simple testing response with proper render

--- views.py
+++ views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello from index view")
+    return render(request,
+                  "mothulity/index.html.jj2",
+                  {"msg": "rendered by index view"})
