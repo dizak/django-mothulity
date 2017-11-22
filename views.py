885c771b956b7542638a516e3416f82ef66ea707
dizak <dariusz.izak@ibb.waw.pl>
2017-11-22 15:10:55 +0100

    ENH:Routing from url to view works

--- views.py
+++ views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello from index view")
