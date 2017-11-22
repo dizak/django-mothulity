6c0274ef6d9dbe8421e6eadbc778cf4c7de8a919
dizak <dariusz.izak@ibb.waw.pl>
2017-11-22 17:42:10 +0100

    ENH:Class for testing views. Func for testing index view response code 200

--- tests.py
+++ tests.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
+from django.urls import reverse
+from . import views
 
 # Create your tests here.
+
+
+class IndexViewTests(TestCase):
+    """
+    Tests for the index view
+    """
+    def test_response_code(self):
+        response = self.client.get(reverse("mothulity:index"))
+        self.assertIs(response.status_code, 200)
