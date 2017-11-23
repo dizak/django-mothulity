d3800b5fc547ffa420ed5d17ca011331f4cd5279
dizak <dariusz.izak@ibb.waw.pl>
2017-11-23 17:09:53 +0100

    ENH:Form evaluation and index or options routing with testing file names passing

--- views.py
+++ views.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
+from mothulity.forms import FileFieldForm
 
 # Create your views here.
 
 
 def index(request):
+    if request.method == "POST":
+        form = FileFieldForm(request.POST,
+                             request.FILES)
+        if form.is_valid():
+            return render(request,
+                          "mothulity/options.html.jj2",
+                          {"methods": request.FILES.getlist("file_field")})
+    else:
+        form = FileFieldForm()
     return render(request,
                   "mothulity/index.html.jj2",
-                  {"msg": "rendered by index view"})
+                  {"form": form})
 
 
 def options(request):
