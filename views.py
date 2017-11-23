c5180598ab436ebf154668d53341e32166102c37
dizak <dariusz.izak@ibb.waw.pl>
2017-11-23 17:34:16 +0100

    ENH:Saving files to path specified in settings.py

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from mothulity.forms import FileFieldForm
 
@@ -11,6 +12,14 @@ def index(request):
         form = FileFieldForm(request.POST,
                              request.FILES)
         if form.is_valid():
+            upld_files = request.FILES.getlist("file_field")
+            print settings.MEDIA_URL
+            for upfile in upld_files:
+                with open("/{}/{}".format(settings.MEDIA_URL,
+                                          upfile),
+                          "wb+") as fout:
+                    for chunk in upfile.chunks():
+                        fout.write(chunk)
             return render(request,
                           "mothulity/options.html.jj2",
                           {"methods": request.FILES.getlist("file_field")})
