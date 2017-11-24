906239ef763c04569cb7626377ca853cbca64e52
dizak <dariusz.izak@ibb.waw.pl>
2017-11-24 19:44:34 +0100

    ENH:OptionsForm class for setting mothulity options

--- forms.py
+++ forms.py
@@ -3,3 +3,10 @@ from django import forms
 
 class FileFieldForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={"multiple": True}))
+
+
+class OptionsForm(forms.Form):
+    job_name = forms.CharField(label="job-name",
+                               max_length=20)
+    notify_email = forms.CharField(label="notify-email",
+                                   max_length=40)
