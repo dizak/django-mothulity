b6f39da68a779a167488e68b96def6587d81dcbc
dizak <dariusz.izak@ibb.waw.pl>
2017-11-24 10:32:45 +0100

    UPD@ViewsResponseTests:Moved setUp as func into this class

--- tests.py
+++ tests.py
@@ -4,26 +4,23 @@ from django.test import TestCase
 from django.urls import reverse
 from . import views
 
-# Create your tests here.
 
-
-class setUp(TestCase):
-    """
-    Sets up class level attributes for the tests.
-
-    Parameters
-    -------
-    urls_list: list of str
-        List urls postfixes to be tested by django's test client.
-    """
-    urls_list = ["index",
-                 "options"]
-
-
-class ViewsResponseTests(setUp):
+class ViewsResponseTests(TestCase):
     """
     Tests for the index view
     """
+    def setUp(self):
+        """
+        Sets up class level attributes for the tests.
+
+        Parameters
+        -------
+        urls_list: list of str
+            List urls postfixes to be tested by django's test client.
+        """
+        self.urls_list = ["index",
+                          "options"]
+
     def test_response_code(self):
         for url in self.urls_list:
             response = self.client.get(reverse("mothulity:{}".format(url)))
