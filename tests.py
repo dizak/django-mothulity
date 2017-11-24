2e87932a4f088fb7343f80ee204784255b181d58
dizak <dariusz.izak@ibb.waw.pl>
2017-11-24 14:51:00 +0100

    ENH:MultipleFilesFormTest for testing files upload

--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 from django.test import TestCase
 from django.urls import reverse
+from django.conf import settings
+from io import BytesIO
 from . import views
 
 
@@ -28,3 +30,41 @@ class ViewsResponseTests(TestCase):
         for url in self.urls_list:
             response = self.client.get(reverse("mothulity:{}".format(url)))
             self.assertIs(response.status_code, 200)
+
+
+class MultipleFilesFormTest(TestCase):
+    """
+    Tests for the multiple files upload on the index view.
+    """
+    def setUp(self):
+        """
+        Sets up class level attributes for the MultipleFilesFormTest test.
+
+        Parameters
+        -------
+        upload_dir: str
+            Directory from django.conf.settings.MEDIA_URL which servers as
+            the file upload directory.
+        test_content: int, 1
+            Content of the uploaded test file. Due to potential problems with
+            str encoding it is simply <1>.
+        test_file: BytesIO object.
+            Mimics the file that would be normally saved on the drive.
+        test_file.name: str
+            Name of the test_file artificial object.
+        """
+        self.upload_dir = settings.MEDIA_URL
+        self.test_content = 1
+        self.test_file = BytesIO(b"{}".format(self.test_content))
+        self.test_file.name = "artificial_file.dat"
+
+    def test_form(self):
+        """
+        Tests whether uploading file into a form using POST method is
+        succesful.
+        """
+        self.client.post("/mothulity/", {"file_field": self.test_file})
+        with open("{}/{}".format(self.upload_dir,
+                                 self.test_file.name)) as fin:
+            self.assertIs(int(fin.read()),
+                          self.test_content)
