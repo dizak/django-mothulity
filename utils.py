656d25ca945ef8e11d8b9f620d387d4979f11494
dariusz.izak@gmail.com <dariusz.izak@gmail.com>
2017-11-25 12:21:36 +0100

    ENH:Func chmod upload file to 400

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
+import os
+
+
 def write_file(input_file,
                path):
     with open("/{}/{}".format(path, input_file), "wb+") as fout:
         for chunk in input_file.chunks():
             fout.write(chunk)
+
+
+def chmod_file(input_file,
+	           mod=400):
+	os.system("chmod {}".format(mod))
