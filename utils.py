70935e029347f3ce666ddf7fc4811864adcb7232
dizak <dariusz.izak@ibb.waw.pl>
2017-11-27 11:13:15 +0100

    ENH:Func for checking if uploaded files are actually fastq

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+from skbio.io import sniff
 
 
 def write_file(input_file,
@@ -12,3 +13,28 @@ def chmod_file(input_file,
                mod=400):
     os.system("chmod {} {}".format(mod,
                                    input_file))
+
+
+def sniff_file(input_file,
+               file_frmt="fastq"):
+    """
+    Returns bool depepending whether file validates as given type.
+
+    Parameters
+    -------
+    input_file: str
+        Path to input file.
+    file_frmt: str
+        File type to be validated as.
+
+    Returns
+    ------
+    bool
+        True if file format matches desired file format or False if it does not.
+
+    """
+    try:
+        if sniff(input_file)[0] == file_frmt:
+            return True
+    except Exception as e:
+        return False
