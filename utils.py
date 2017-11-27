a17d8a63a4792b7ae4a99f74a5ce79bf12a0d317
dizak <dariusz.izak@ibb.waw.pl>
2017-11-27 11:20:09 +0100

    UPD:__doc__

--- utils.py
+++ utils.py
@@ -4,6 +4,16 @@ from skbio.io import sniff
 
 def write_file(input_file,
                path):
+    """
+    Saves file from the forms.FileFieldForm to desired path in chunks.
+
+    Parameters
+    -------
+    input_file: str
+        Name of the input file.
+    path: str
+        Path to file save.
+    """
     with open("/{}/{}".format(path, input_file), "wb+") as fout:
         for chunk in input_file.chunks():
             fout.write(chunk)
@@ -11,6 +21,17 @@ def write_file(input_file,
 
 def chmod_file(input_file,
                mod=400):
+    """
+    Chmods file uploaded with forms.FileFieldForm. It is ment to prevent
+    execution of uploaded files.
+
+    Parameters
+    -------
+    input_file: str
+        Path to input file.
+    mod: int or str, default <400>
+        Desired permissions of the input_file
+    """
     os.system("chmod {} {}".format(mod,
                                    input_file))
 
@@ -24,7 +45,7 @@ def sniff_file(input_file,
     -------
     input_file: str
         Path to input file.
-    file_frmt: str
+    file_frmt: str, default <fastq>
         File type to be validated as.
 
     Returns
