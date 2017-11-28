3273139a8cca5da9078b98f6ad5a9778ea4f1e67
dizak <dariusz.izak@ibb.waw.pl>
2017-11-28 17:16:43 +0100

    ENH:Model for saving job_id

--- models.py
+++ models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class JobID(models.Model):
+    job_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.job_id
