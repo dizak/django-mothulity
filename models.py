4246d4921b483113792fcff62543a35f94dedc57
dizak <dariusz.izak@ibb.waw.pl>
2017-11-29 18:23:39 +0100

    ENH:Proper passing values from from to db

--- models.py
+++ models.py
@@ -11,3 +11,23 @@ class JobID(models.Model):
 
     def __str__(self):
         return self.job_id
+
+
+class SubmissionData(models.Model):
+    job_id = models.ForeignKey(JobID,
+                               on_delete=models.CASCADE)
+    job_name = models.CharField(max_length=20)
+    notify_email = models.CharField(max_length=40)
+    max_ambig = models.IntegerField()
+    max_homop = models.IntegerField()
+    min_length = models.IntegerField()
+    max_length = models.IntegerField()
+    min_overlap = models.IntegerField()
+    screen_criteria = models.IntegerField()
+    chop_length = models.IntegerField()
+    precluster_diffs = models.IntegerField()
+    classify_seqs_cutoff = models.IntegerField()
+    amplicon_type = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.job_name
--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from mothulity.forms import FileFieldForm, OptionsForm
-from mothulity.models import JobID
+from mothulity.models import JobID, SubmissionData
 import utils
 import uuid
 
@@ -47,6 +47,20 @@ def submit(request):
     job_id = uuid.uuid4()
     job_id_link = "{}{}".format(request.build_absolute_uri(),
                                 job_id)
+    job = JobID(job_id=job_id)
+    job.save()
+    sub_data = job.submissiondata_set.create(job_name=request.POST["job_name"],
+                                             notify_email=request.POST["notify_email"],
+                                             max_ambig=request.POST["max_ambig"],
+                                             max_homop=request.POST["max_homop"],
+                                             min_length=request.POST["min_length"],
+                                             max_length=request.POST["max_length"],
+                                             min_overlap=request.POST["min_overlap"],
+                                             screen_criteria=request.POST["screen_criteria"],
+                                             chop_length=request.POST["chop_length"],
+                                             precluster_diffs=request.POST["precluster_diffs"],
+                                             classify_seqs_cutoff=request.POST["classify_seqs_cutoff"],
+                                             amplicon_type=request.POST["amplicon_type"])
     return render(request,
                   "mothulity/submit.html.jj2",
                   {"notify_email": request.POST["notify_email"],
