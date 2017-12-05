639e1da70310f2b3081eadc090c6397290d0d880
dizak <dariusz.izak@ibb.waw.pl>
2017-12-05 14:43:16 +0100

    ENH:Func parse_sinfo for getting desired values from whole sinfo output

--- sched.py
+++ sched.py
@@ -2,4 +2,29 @@
 
 
 import schedule
+import subprocess as sp
 from mothulity import models
+
+
+def parse_sinfo(input_str,
+                partition,
+                state):
+    """
+    Parse sinfo command output and return desired information.
+
+    Parameters
+    -------
+    input_file: str
+        Path to input file.
+    partition: str
+        Partition of desired nodes.
+    state: str
+        State of desired nodes.
+
+    Return
+    -------
+    int
+        Number of nodes in desired state and partition.
+    """
+    s_line = [i for i in input_str.split("\n") if partition in i and state in i]
+    return int(s_line[0].split()[3])
