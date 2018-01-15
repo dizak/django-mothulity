f16b3a32d240e8ec6b39e495ecb1c04a194e40a7
dizak <dariusz.izak@ibb.waw.pl>
2018-01-15 11:29:45 +0100

    ENH:Url to wiki arts

--- urls.py
+++ urls.py
@@ -6,4 +6,5 @@ app_name = "mothulity"
 
 urlpatterns = [url(r"^$", views.index, name="index"),
                url(r"^submit/(?P<job>.+)$", views.submit, name="submit"),
-               url(r"^status/(?P<job>.+)$", views.status, name="status")]
+               url(r"^status/(?P<job>.+)$", views.status, name="status"),
+               url(r"^wiki/(?P<title>.+)$", views.wiki, name="wiki")]
